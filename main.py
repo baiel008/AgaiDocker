197d4aec7ea3a40c07c07e8c58c16482641f6adc
ba1ai.ql <joldoshbekovbaiel6@gmail.com>
2025-12-28 18:30:13 +0600

    LALA

--- main.py
+++ main.py
@@ -13,29 +13,49 @@ CLASSES = [
 class CifarClassifaction(nn.Module):
     def __init__(self):
         super().__init__()
-        self.first = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
 
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
+        def conv_block(in_f, out_f):
+            return nn.Sequential(
+                nn.Conv2d(in_f, out_f, kernel_size=3, padding=1),
+                nn.BatchNorm2d(out_f),
+                nn.ReLU(inplace=True)
+            )
 
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
+        self.features = nn.Sequential(
+            # Block 1
+            conv_block(3, 64), conv_block(64, 64),
+            nn.MaxPool2d(2, 2),
+            # Block 2
+            conv_block(64, 128), conv_block(128, 128),
+            nn.MaxPool2d(2, 2),
+            # Block 3
+            conv_block(128, 256), conv_block(256, 256),
+            conv_block(256, 256), conv_block(256, 256),
+            nn.MaxPool2d(2, 2),
+            # Block 4
+            conv_block(256, 512), conv_block(512, 512),
+            conv_block(512, 512), conv_block(512, 512),
+            nn.MaxPool2d(2, 2),
+            # Block 5
+            conv_block(512, 512), conv_block(512, 512),
+            conv_block(512, 512), conv_block(512, 512),
+            nn.MaxPool2d(2, 2),
         )
-        self.second = nn.Sequential(
+
+        self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(128*4*4, 256),
-            nn.ReLU(),
-            nn.Linear(256, 10)
+            nn.Linear(512 * 1 * 1, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(512, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(512, 10)
         )
 
     def forward(self, x):
-        x = self.first(x)
-        x = self.second(x)
+        x = self.features(x)
+        x = self.classifier(x)
         return x
 
 
